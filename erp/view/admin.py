e2518adf72813db78d4b7139b57f1d0ba5c6567d
faisalburhanudin <faisalburhanudin@gmail.com>
2018-05-29 13:48:14 +0700

    presence view

--- erp/view/admin.py
+++ erp/view/admin.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint("admin", __name__)
+bp = Blueprint("admin", __name__, url_prefix='/admin')
 
 
-@bp.route('/admin')
+@bp.route('/')
 def admin():
     return render_template('admin/home.html')
+
+
+@bp.route('/presence')
+def presence():
+    return render_template('admin/presence.html')
--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 from erp.app import app
-app.run()
+app.run(debug=True)
