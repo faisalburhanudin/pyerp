97ec9bd203ada1988003be0b0be86e6dbbe52df5
faisalburhanudin <faisalburhanudin@gmail.com>
2018-05-29 14:58:00 +0700

    employee add and edit view

--- erp/view/admin.py
+++ erp/view/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from erp.model import Users
 
 bp = Blueprint("admin", __name__, url_prefix='/admin')
@@ -21,3 +21,17 @@ def employee():
         'admin/employee/list.html',
         users=users
     )
+
+
+@bp.route('/employee/new')
+def employee_add():
+    return render_template('admin/employee/new.html')
+
+
+@bp.route('/employee/edit')
+def employee_edit():
+    user_id = request.args.get('id')
+    user = Users.query.filter_by(
+        id=user_id
+    ).first()
+    return render_template('admin/employee/edit.html', user=user)
