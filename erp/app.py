70092a75a55c8e2c54b8fab2dc535416c9d03f03
faisalburhanudin <faisalburhanudin@gmail.com>
2018-05-30 08:26:42 +0700

    fix circural dependencies

--- erp/app.py
+++ erp/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from erp.view import admin
 from erp.model import db
-from erp.auth import login_manager, bcrypt
+from flask_login import LoginManager
+from erp.model import Users
+from erp.auth import bcrypt
 
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/erp'
+
+
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.filter_by(id=user_id).first()
 
 
 app = Flask(__name__)
--- erp/auth.py
+++ erp/auth.py
@@ -1,12 +1,3 @@
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
-from erp.model import Users
-
-login_manager = LoginManager()
 bcrypt = Bcrypt()
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return Users.query.filter_by(id=user_id).first()
