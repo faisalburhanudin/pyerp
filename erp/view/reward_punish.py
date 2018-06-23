002bcc7e499f6f912a474b26feb2d3b12e14a2b6
faisalburhanudin <faisalburhanudin@gmail.com>
2018-06-23 13:43:06 +0700

    edit reward

--- erp/view/reward_punish.py
+++ erp/view/reward_punish.py
@@ -36,6 +36,29 @@ def add_reward():
     return render_template('admin/reward_and_punish/add_reward.html')
 
 
+@bp.route('/edit-reward', methods=["POST", "GET"])
+def edit_reward():
+    if request.method == "POST":
+        reward_id = request.form.get("id")
+        name = request.form.get("name")
+
+        reward = RewardPunish.query.filter_by(id=reward_id).first()
+        reward.name = name
+
+        db.session.add(reward)
+        db.session.commit()
+
+        return redirect('/admin/reward-punish')
+
+    reward_id = request.args.get("id")
+    reward = RewardPunish.query.filter_by(id=reward_id).first()
+
+    return render_template(
+        'admin/reward_and_punish/edit_reward.html',
+        reward=reward
+    )
+
+
 @bp.route('/add-punish', methods=['GET', 'POST'])
 def add_punish():
     if request.method == "POST":
